cfc1c61d5c09e845e63aa443e058c4020a51dff2
Mario Mey <mariomey@gmail.com>
2021-07-29 12:25:38 -0300

    Update obs_osc_pythonscript.py

--- obs_osc_pythonscript.py
+++ obs_osc_pythonscript.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 OBS-OSC-PythonScript 0.1 (https://github.com/MarioMey/obs-osc-pythonscript)
 
@@ -31,23 +32,32 @@ from obs_osc_api import (
 	th
 )
 
-from obs_osc_api import (
+from obs_api import (
+	scene_change, 
 	item_set_visible,
 	item_remove, 
-	item_tween, 
 	item_duplicate,
 	item_reference,
-	item_get_transform,
 	item_set_transform,
+	item_get_transform,
 	item_set_pos,
 	item_set_scl,
 	item_set_rot,
 	item_set_alignment,
 	item_set_visible,
-	item_set_crop
+	item_set_crop,
+	item_set_scl_filter,
+	item_get_order_index,
+	item_set_order_index,
+	item_swap_order_index,
+	item_set_private_setting,
+	item_create_group,
+	item_create_text,
+	item_create_image,
+	item_create_video,
 )
 
-from obs_osc_api import (
+from obs_api import (
 	source_set_image_file, 
 	source_set_video_file, 
 	source_set_slide_time,
@@ -58,10 +68,19 @@ from obs_osc_api import (
 	source_set_bri_sat_hue, 
 	source_set_hue, 
 	source_set_lut_file,
+	source_set_setting,
 	source_filter_set_value, 
 	source_filter_set_enabled, 
+	source_filter_get_settings, 
 	source_set_text,
-	source_get_settings
+	source_capture_window, 
+	source_get_settings, 
+	source_capture_window
+)
+
+from obs_tween import (
+	item_tween, 
+	source_tween
 )
 
 from pythonosc import osc_message_builder
@@ -90,7 +109,30 @@ def source_activated(cd):
 def handleOSC(address, args, data):
 	print(f'{address}, {args}, {data}')
 
-def tween(*args):
+def source_tween_(*args):
+	
+	if args[1] in ['to_value', 'value']:
+		source_tween(None,
+			source_name=args[2],
+			filter_name=args[3],
+			setting=args[4],
+			to_value=args[5],
+			duration=args[6],
+			delay=args[7] if len(args) > 7 else 0,
+			ease_type=args[8] if len(args) > 8 else 'linear')
+
+	elif args[1] == 'from_to_value':
+		source_tween(None, 
+			source_name=args[2],
+			filter_name=args[3],
+			setting=args[4],
+			from_value=args[5],
+			to_value=args[6],
+			duration=args[7],
+			delay=args[8] if len(args) > 8 else 0,
+			ease_type=args[9] if len(args) > 9 else 'linear')
+
+def item_tween_(*args):
 
 	if args[1] in ['to_pos', 'pos']:
 		item_tween(None,
@@ -217,33 +259,47 @@ def script_load(settings):
 
 	despachante.map("/scene_change", scene_change)
 
-	despachante.map("/item_set_visible",   item_set_visible)
-	despachante.map("/item_remove",        item_remove)
-	despachante.map("/item_tween",         tween)
-	despachante.map("/item_duplicate",     item_duplicate)
-	despachante.map("/item_reference",     item_reference)
-	despachante.map("/item_set_transform", item_set_transform)
-	despachante.map("/item_get_transform", item_get_transform)
-	despachante.map("/item_set_pos",       item_set_pos)
-	despachante.map("/item_set_scl",       item_set_scl)
-	despachante.map("/item_set_rot",       item_set_rot)
-	despachante.map("/item_set_alignment", item_set_alignment)
-	despachante.map("/item_set_visible",   item_set_visible)
-	despachante.map("/item_set_crop",      item_set_crop)
+	despachante.map("/item_set_visible",         item_set_visible)
+	despachante.map("/item_remove",              item_remove)
+	despachante.map("/item_tween",               item_tween_)
+	despachante.map("/item_duplicate",           item_duplicate)
+	despachante.map("/item_reference",           item_reference)
+	despachante.map("/item_get_transform",       item_get_transform)
+	despachante.map("/item_set_transform",       item_set_transform)
+	despachante.map("/item_set_pos",             item_set_pos)
+	despachante.map("/item_set_scl",             item_set_scl)
+	despachante.map("/item_set_rot",             item_set_rot)
+	despachante.map("/item_set_alignment",       item_set_alignment)
+	despachante.map("/item_set_visible",         item_set_visible)
+	despachante.map("/item_set_crop",            item_set_crop)
+	despachante.map("/item_get_order_index",     item_get_order_index)
+	despachante.map("/item_set_order_index",     item_set_order_index)
+	despachante.map("/item_swap_order_index",    item_swap_order_index)
+	despachante.map("/item_set_private_setting", item_set_private_setting)
+	despachante.map("/item_set_scl_filter",      item_set_scl_filter)
+	despachante.map("/item_create_group",        item_create_group)
+	despachante.map("/item_create_text",         item_create_text)
+	despachante.map("/item_create_image",        item_create_image)
+	despachante.map("/item_create_video",        item_create_video)
 
-	despachante.map("/source_set_image_file",     source_set_image_file)
-	despachante.map("/source_set_video_file",     source_set_video_file)
-	despachante.map("/source_set_slide_time",     source_set_slide_time)
-	despachante.map("/source_set_text",           source_set_text)
-	despachante.map("/source_set_text_size",      source_set_text_size)
-	despachante.map("/source_set_volume",         source_set_volume)
-	despachante.map("/source_set_opacity",        source_set_opacity)
-	despachante.map("/source_filter_set_enabled", source_filter_set_enabled)
-	despachante.map("/source_filter_set_value",   source_filter_set_value)
-	despachante.map("/source_set_bri_sat_hue",    source_set_bri_sat_hue)
-	despachante.map("/source_set_hue",            source_set_hue)
-	despachante.map("/source_set_lut_file",       source_set_lut_file)
-	despachante.map("/source_get_settings",       source_get_settings)
+
+	despachante.map("/source_tween",               source_tween_)
+	despachante.map("/source_set_image_file",      source_set_image_file)
+	despachante.map("/source_set_video_file",      source_set_video_file)
+	despachante.map("/source_set_slide_time",      source_set_slide_time)
+	despachante.map("/source_set_text",            source_set_text)
+	despachante.map("/source_set_text_size",       source_set_text_size)
+	despachante.map("/source_set_volume",          source_set_volume)
+	despachante.map("/source_set_opacity",         source_set_opacity)
+	despachante.map("/source_filter_set_enabled",  source_filter_set_enabled)
+	despachante.map("/source_filter_set_value",    source_filter_set_value)
+	despachante.map("/source_filter_get_settings", source_filter_get_settings)
+	despachante.map("/source_set_setting",         source_set_setting)
+	despachante.map("/source_set_bri_sat_hue",     source_set_bri_sat_hue)
+	despachante.map("/source_set_hue",             source_set_hue)
+	despachante.map("/source_set_lut_file",        source_set_lut_file)
+	despachante.map("/source_capture_window",      source_capture_window)
+	despachante.map("/source_get_settings",        source_get_settings)
 	# despachante.map("/*", handleOSC)
 
 
